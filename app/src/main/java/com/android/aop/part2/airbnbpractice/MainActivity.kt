de8767514785e3e95b492f1a223ff54ba5273c82
Dustin_Jung <jadebejung@gmail.com>
2022-04-30 12:23:54 +0900

    Airbnb Practice

--- app/src/main/java/com/android/aop/part2/airbnbpractice/MainActivity.kt
+++ app/src/main/java/com/android/aop/part2/airbnbpractice/MainActivity.kt
@@ -3,12 +3,18 @@ package com.android.aop.part2.airbnbpractice
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.android.aop.part2.airbnbpractice.databinding.ActivityMainBinding
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.FusedLocationSource
 import com.naver.maps.map.util.MarkerIcons
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -41,11 +47,43 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         locationSource = FusedLocationSource(this@MainActivity,100)
         naverMap.locationSource = locationSource
 
-        val marker = Marker()
-        marker.position = LatLng(37.498095,127.027610)
-        marker.map = naverMap
-        marker.icon = MarkerIcons.BLACK
-        marker.iconTintColor = Color.RED
+
+
+        getHouseListFromAPI()
+    }
+
+    private fun getHouseListFromAPI(){
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://run.mocky.io/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(HouseService::class.java).also {
+            it.getHouseList()
+                .enqueue(object : Callback<HouseDto>{
+                    override fun onResponse(call: Call<HouseDto>, response: Response<HouseDto>) {
+                        if(response.isSuccessful.not()){
+                            return // 실패처리에 대한 구현
+                        }
+
+                        response.body()?.let { dto->
+                            dto.items.forEach{ model ->
+                                val marker = Marker()
+                                marker.position = LatLng(model.lat,model.lng)
+                                marker.map = naverMap
+                                marker.tag = model.id
+                                marker.icon = MarkerIcons.BLACK
+                                marker.iconTintColor = Color.RED
+                            }
+                        }
+                    }
+
+                    override fun onFailure(call: Call<HouseDto>, t: Throwable) {
+                        //실패처리에 대한 구현
+                    }
+                })
+        }
+
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -66,6 +104,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     companion object{
         private const val LOCATION_PERMISSION_REQUEST_CODE = 100
     }
+
 
     override fun onStart() {
         super.onStart()
